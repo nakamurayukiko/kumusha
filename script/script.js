68568bb15db19ea54cf519079850c502ed80a94e
nakamurayukiko <66461965+nakamurayukiko@users.noreply.github.com>
2021-07-27 02:56:17 +0900

    ファイルアップロード

---
+++ script/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.hamburger').click(function() {
+    $(this).toggleClass('active');
+
+    if ($(this).hasClass('active')) {
+        $('.globalMenuSp').addClass('active');
+    } else {
+        $('.globalMenuSp').removeClass('active');
+    }
+  }); 
+});
